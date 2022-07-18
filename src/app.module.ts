e1639d1382d85de2cd6ab2d2b92849304ff6f059
Michael Focacci <michaelfocacci@gmail.com>
2022-07-17 21:01:35 -0400

    Config with env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { AppService } from './app.service';
 import { GroceryModule } from './grocery/grocery.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroceryItem } from './grocery/entities/grocery-item.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'michaelfocacci',
-      password: 'tunafish',
-      database: 'homeserver',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [GroceryItem],
       synchronize: true // not for prod
     }),
